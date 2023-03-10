920702c8992a09c3072deffeaa455ece547db72f
Icepick <remi.jara@laposte.net>
2023-03-10 14:46:42 +0100

    adding routing in the project

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,45 +1,55 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
 
 <template>
-    <div class="flex flex-row w-screen justify-between h-20 bg-white">
+    <div class="flex flex-row w-screen justify-between h-auto bg-white">
         <div class="flex flex-row justify-center items-center">
             <div class="m-4">
-                <a
-                    href="#"
-                    class="font-medium text-blue-900 no-underline hover:text-blue-600"
-                    >Home</a
+                <RouterLink
+                    to="/"
+                    class="font-medium text-blue-900 no-underline hover:text-blue-600 text-xl"
                 >
+                    Home
+                </RouterLink>
+            </div>
+            <div class="m-4">
+                <RouterLink
+                    to="/about"
+                    class="font-medium text-blue-900 no-underline hover:text-blue-600 text-xl"
+                >
+                    About
+                </RouterLink>
             </div>
             <div class="m-4">
                 <a
                     href="#"
-                    class="font-medium text-blue-900 no-underline hover:text-blue-600"
-                    >About</a
-                >
-            </div>
-            <div class="m-4">
-                <a
-                    href="#"
-                    class="font-medium text-blue-900 no-underline hover:text-blue-600"
+                    class="font-medium text-blue-900 no-underline hover:text-blue-600 text-xl"
                     >Countries</a
                 >
             </div>
         </div>
-        <div class="m-4 flex items-center">
-            <img src="../assets/logo.png" alt="logo" />
+        <div class="flex items-center">
+            <RouterLink to="/">
+                <img
+                    src="../assets/logo/logo1.png"
+                    alt="logo"
+                    class="w-28 h-28"
+                />
+            </RouterLink>
         </div>
         <div class="flex flex-row justify-center items-center">
             <div class="m-4">
                 <a
                     href="#"
-                    class="font-medium text-blue-900 no-underline hover:text-blue-600"
+                    class="font-medium text-blue-900 no-underline hover:text-blue-600 text-xl"
                     >Sign In</a
                 >
             </div>
             <div class="m-4">
                 <a
                     href="#"
-                    class="font-medium text-blue-900 no-underline hover:text-blue-600"
+                    class="font-medium text-blue-900 no-underline hover:text-blue-600 text-xl"
                     >Sign Up</a
                 >
             </div>
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+    name: 'About',
+    components: {},
+    setup() {
+        return {};
+    }
+};
+</script>
+
+<template>
+    <h1 class="text-white">Hello About !</h1>
+</template>
+
+<style scoped></style>
