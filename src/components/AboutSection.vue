365ee56db574c814ab24b9b8e901fcf92e5aa91b
Icepick <remi.jara@laposte.net>
2023-03-22 22:41:43 +0100

    finished about page with all content

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -24,22 +24,23 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="grid grid-rows-2 gap-5">
-        <div class="flex flex-col justify-center items-center">
-            <img :src="image" :alt="alt" class="w-1/2" />
-            <h1 class="text-2xl font-bold text-center text-black">
-                {{ title }}
-            </h1>
+    <div
+        class="grid grid-cols-1 sm:grid-cols-2 gap-5 w-5/6 lg:w-4/6 xl:w-1/2 items-center"
+    >
+        <div class="flex flex-col gap-5">
+            <h1 class="text-4xl font-bold">{{ title }}</h1>
+            <p class="text-gray-600">{{ text }}</p>
+            <RouterLink :to="link" class="text-primary hover:underline text-lg"
+                >Learn more</RouterLink
+            >
         </div>
-        <p class="text-center text-black">{{ text }}</p>
-        <div class="flex justify-center">
-            <Routerlink :to="link">
-                <button
-                    class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded"
-                >
-                    Learn more
-                </button>
-            </Routerlink>
+        <div class="relative">
+            <img :src="image" :alt="link.substring(1)" class="w-full h-auto" />
+            <span
+                class="absolute -bottom-5 right-0 text-black text-sm"
+                v-html="alt"
+            >
+            </span>
         </div>
     </div>
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -17,12 +17,12 @@ import AboutSection from '../components/AboutSection.vue';
                 Capitalympics
             </span>
             <span class="right-1 bottom-0 text-white absolute text-sm">
-                Photo de
+                Photo of
                 <a
                     href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
                     >NASA</a
                 >
-                sur
+                on
                 <a
                     href="https://unsplash.com/fr/photos/Q1p7bh3SHj8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
                     >Unsplash</a
@@ -43,21 +43,55 @@ import AboutSection from '../components/AboutSection.vue';
                 nations.
             </p>
         </div>
-        <div class="flex flex-col bg-white">
+        <div
+            class="flex flex-col bg-white justify-center items-center gap-20 pt-10 pb-10 w-full"
+        >
             <AboutSection
-                title="📈 Improve Your Skills with Personalized Learning"
+                title="📈 Improve Your Skills with Personalized Learning (soon)"
                 text="At Capitalympics, we understand that everyone learns at their own pace. That's why our app uses advanced algorithms to track your progress and provide personalized recommendations based on your individual needs. With Capitalympics, you can improve your skills and master the capitals and flags of the world."
-                image="../assets/about/learn.jpg"
-                alt="learn"
+                image="src/assets/about/learn.jpg"
+                alt='Photo of Lukas on <a href="https://www.pexels.com/fr-fr/photo/personne-tenant-un-stylo-pointant-sur-le-graphique-590020/">Pexels</a>'
                 link="/learn"
             />
             <AboutSection
-                title="Play"
-                text="Play our fun and challenging games to test your knowledge."
-                image="../assets/about/play.jpg"
-                alt="play"
-                link="/play"
+                title="🔍 Filter and Sort Countries to Learn More"
+                text="Our app features powerful search and filtering capabilities that allow users to find the information they need quickly and easily. Whether you're looking for a specific country or want to explore a particular region of the world, Capitalympics makes it simple."
+                image="src/assets/about/countries.jpg"
+                alt='Photo of <a href="https://unsplash.com/@jannerboy62?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nick Fewings</a> on <a href="https://unsplash.com/fr/photos/BAZejJdZ57w?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'
+                link="/countries"
             />
+            <AboutSection
+                title="🎮 Play the Capitalympics Quiz Game (soon)"
+                text="Learning doesn't have to be boring! That's why we've created a fun and engaging quiz game that lets you test your knowledge of the world's capitals and flags. With multiple difficulty levels you can track your progress, the Capitalympics Quiz Game is the perfect way to add some excitement to your learning experience."
+                image="src/assets/about/quiz.jpg"
+                alt='Photo of <a href="https://unsplash.com/@ingvar_erik?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Igor Karimov 🇺🇦 </a> on <a href="https://unsplash.com/fr/photos/M1nZU61xTK4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'
+                link="/quiz"
+            />
+            <AboutSection
+                title="🏆 Become a Capitalympics Champion"
+                text="Our app is designed to help you achieve your learning goals and become a Capitalympics champion. With each correct answer, you'll earn points and increase your overall score."
+                image="src/assets/about/trophy.jpg"
+                alt='Photo of RODNAE Productions on <a href="https://www.pexels.com/fr-fr/photo/concept-succes-premier-meilleur-7005687/"> Pexels</a>'
+                link="/profile"
+            />
+            <AboutSection
+                title="👨‍💻 Meet the Developer"
+                text="Capitalympics was created by a passionate student developer who loves learning about different countries and cultures. As someone who has always been fascinated by flags and capitals, he wanted to create an app that would make it easy for others to learn as well."
+                image="src/assets/about/developer.jpg"
+                alt='Photo of Lukas on <a href="https://www.pexels.com/fr-fr/photo/stylo-retractable-bleu-574070/">Pexels</a> '
+                link="/developer"
+            />
+            <AboutSection
+                title="💻 Open-Source Development"
+                text="If you're interested in checking out the code behind Capitalympics or contributing to the project, you can find the repo on GitHub. The app is built using a variety of cutting-edge technologies, and the developer is always looking for ways to improve it and make it even more fun and engaging for users."
+                image="src/assets/about/github.jpg"
+                alt='Photo of Soumil Kumar on <a href="https://www.pexels.com/fr-fr/photo/photo-de-personne-tapant-sur-le-clavier-de-l-ordinateur-735911/">Pexels</a>'
+                link="https://github.com/icepick4/capitalympics"
+            />
+            <h1 class="text-3xl text-center mt-10 lg:w-1/2">
+                We hope you enjoy using Capitalympics as much as we enjoyed
+                creating it. Happy learning! 🌍🚀
+            </h1>
         </div>
     </div>
 </template>
