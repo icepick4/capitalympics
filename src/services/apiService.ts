2ae798332b310bad30721aae805d4824649fd5f2
Icepick <remi.jara@laposte.net>
2023-07-03 23:26:59 +0200

    adding api endpoint in env

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,7 +3,7 @@ import { CountryI } from '../models/Country';
 import { Level, User, UserScore } from '../models/User';
 import { Lang, LearningType, Region, ScoreType } from '../types/common';
 export default class ApiService {
-    public static readonly API_URL: string = 'http://localhost:3001/api';
+    public static readonly API_URL: string = import.meta.env.VITE_API_ENDPOINT;
 
     public static async getCountries(
         max: number = 0,
