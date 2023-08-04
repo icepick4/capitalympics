59067fcf8668ada13b2e691a87286f7463ed61d7
Icepick <remi.jara@laposte.net>
2023-08-04 03:54:37 +0200

    adding chunk plugin from vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import compression from 'vite-plugin-compression';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        splitVendorChunkPlugin(),
         vue(),
         compression(),
         VitePWA({
@@ -58,7 +59,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
-    },
+            '@': path.resolve(__dirname, './src')
+        }
+    }
 });
