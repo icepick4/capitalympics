e73851a73a91ec2b8b76556ca50e83335f71da94
Icepick <remi.jara@laposte.net>
2024-01-16 11:53:23 +0100

    trying to center login and signup pbs

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -63,86 +63,88 @@ async function login() {
         :buttonDescription="$t('close')"
         type="error"
     />
-    <section
-        class="relative py-10 bg-gray-900 sm:py-16 lg:py-24 h-full flex items-center"
-    >
-        <div class="absolute inset-0">
-            <img
-                class="object-cover w-full h-full"
-                src="/signup.jpg"
-                alt="background"
-            />
-        </div>
-        <div class="absolute bottom-0 right-1 text-white z-10">
-            Photo of
-            <a
-                href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                >NASA</a
-            >
-            on
-            <a
-                href="https://unsplash.com/fr/photos/6-jTZysYY_U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                >Unsplash</a
-            >
-        </div>
+    <div class="flex h-full w-full justify-center items-center">
+        <section
+            class="relative py-10 bg-gray-900 sm:py-16 lg:py-24 flex items-center h-full w-full"
+        >
+            <div class="absolute inset-0">
+                <img
+                    class="object-cover w-full h-full"
+                    src="/signup.jpg"
+                    alt="background"
+                />
+            </div>
+            <div class="absolute bottom-0 right-1 text-white z-10">
+                Photo of
+                <a
+                    href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                    >NASA</a
+                >
+                on
+                <a
+                    href="https://unsplash.com/fr/photos/6-jTZysYY_U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                    >Unsplash</a
+                >
+            </div>
 
-        <div class="relative max-w-lg px-4 mx-auto sm:px-0">
-            <div
-                class="overflow-hidden bg-gradient rounded-md shadow-md p-0 sm:p-5 lg:p-8"
-            >
-                <div class="px-4 py-6 sm:px-8 sm:py-7">
-                    <div class="text-center">
-                        <h2 class="text-3xl font-bold text-gray-900">
-                            {{ $t('welcomeBack') }}
-                        </h2>
-                        <p class="mt-2 text-base text-black">
-                            {{ $t('noAccount') }}
-                            <RouterLink
-                                to="/signup"
-                                class="text-black hover:underline italic"
+            <div class="relative max-w-lg px-4 mx-auto sm:px-0">
+                <div
+                    class="overflow-hidden bg-gradient rounded-md shadow-md p-0 sm:p-5 lg:p-8"
+                >
+                    <div class="px-4 py-6 sm:px-8 sm:py-7">
+                        <div class="text-center">
+                            <h2 class="text-3xl font-bold text-gray-900">
+                                {{ $t('welcomeBack') }}
+                            </h2>
+                            <p class="mt-2 text-base text-black">
+                                {{ $t('noAccount') }}
+                                <RouterLink
+                                    to="/signup"
+                                    class="text-black hover:underline italic"
+                                >
+                                    {{ $t('createAccount') }}
+                                </RouterLink>
+                            </p>
+                        </div>
+                        <div class="mt-5 space-y-5">
+                            <TextInput
+                                v-model="name"
+                                :label="$t('name')"
+                                :placeholder="$t('namePlaceholder')"
+                                :prepend-icon="IconUser"
+                                @keyup="
+                                    ($event.key === 'Enter' ||
+                                        $event.key === ' ') &&
+                                        login()
+                                "
+                            />
+                            <PasswordInput
+                                v-model="password"
+                                :label="$t('password')"
+                                :placeholder="$t('passwordPlaceholder')"
+                                revelable
+                                @keyup="
+                                    ($event.key === 'Enter' ||
+                                        $event.key === ' ') &&
+                                        login()
+                                "
+                            />
+                            <button
+                                type="button"
+                                :disabled="!isFormFilled"
+                                class="w-full px-4 py-4 flex justify-center items-center text-base font-semibold text-black bg-white rounded-md transition-all duration-200 delay-100 hover:scale-105 focus:scale-105 disabled:hover:scale-100 disabled:opacity-60 disabled:cursor-not-allowed"
+                                @click="login"
                             >
-                                {{ $t('createAccount') }}
-                            </RouterLink>
-                        </p>
-                    </div>
-                    <div class="mt-5 space-y-5">
-                        <TextInput
-                            v-model="name"
-                            :label="$t('name')"
-                            :placeholder="$t('namePlaceholder')"
-                            :prepend-icon="IconUser"
-                            @keyup="
-                                ($event.key === 'Enter' ||
-                                    $event.key === ' ') &&
-                                    login()
-                            "
-                        />
-                        <PasswordInput
-                            v-model="password"
-                            :label="$t('password')"
-                            :placeholder="$t('passwordPlaceholder')"
-                            revelable
-                            @keyup="
-                                ($event.key === 'Enter' ||
-                                    $event.key === ' ') &&
-                                    login()
-                            "
-                        />
-                        <button
-                            type="button"
-                            :disabled="!isFormFilled"
-                            class="w-full px-4 py-4 flex justify-center items-center text-base font-semibold text-black bg-white rounded-md transition-all duration-200 delay-100 hover:scale-105 focus:scale-105 disabled:hover:scale-100 disabled:opacity-60 disabled:cursor-not-allowed"
-                            @click="login"
-                        >
-                            <span v-if="!isLoading">{{ $t('login') }}</span>
-                            <div
-                                v-else
-                                class="h-5 w-5 border-4 rounded-full border-blue-600/75 border-b-blue-600/25 animate-spin"
-                            ></div>
-                        </button>
+                                <span v-if="!isLoading">{{ $t('login') }}</span>
+                                <div
+                                    v-else
+                                    class="h-5 w-5 border-4 rounded-full border-blue-600/75 border-b-blue-600/25 animate-spin"
+                                ></div>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -86,159 +86,183 @@ const validateForm = () => {
         :buttonDescription="$t('close')"
         type="error"
     />
-    <section class="h-full flex w-full justify-center">
-        <div class="grid grid-cols-1 lg:grid-cols-2 w-full">
-            <div
-                class="relative flex items-center px-4 pb-10 pt-10 sm:pb-16 md:justify-center lg:pb-24 sm:px-6 lg:px-8"
-            >
-                <div class="absolute top-0 right-1 text-white z-40">
-                    Photo of
-                    <a
-                        href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                        >NASA</a
-                    >
-                    on
-                    <a
-                        href="https://unsplash.com/fr/photos/6-jTZysYY_U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                        >Unsplash</a
-                    >
-                </div>
-                <div class="absolute inset-0">
-                    <img
-                        class="object-cover w-full h-full"
-                        src="/signup.jpg"
-                        alt="background"
-                    />
-                </div>
+    <div class="flex h-full w-full justify-center items-center">
+        <section class="h-full flex w-full justify-center">
+            <div class="grid grid-cols-1 lg:grid-cols-2 w-full">
                 <div
-                    class="absolute inset-0 bg-gradient-to-t from-black to-transparent"
-                ></div>
-
-                <div class="relative">
-                    <div
-                        class="w-full max-w-xl xl:w-full xl:mx-auto xl:pr-24 xl:max-w-xl"
-                    >
-                        <h3 class="text-4xl font-bold text-white">
-                            {{ numberOfPeople }} {{ $t('peopleUsing') }}
-                            <br class="hidden xl:block" />
-                            Capitalympics!
-                        </h3>
-                        <ul
-                            class="grid grid-cols-1 mt-10 sm:grid-cols-2 gap-x-8 gap-y-4"
+                    class="relative flex items-center px-4 pb-10 pt-10 sm:pb-16 md:justify-center lg:pb-24 sm:px-6 lg:px-8"
+                >
+                    <div class="absolute top-0 right-1 text-white z-40">
+                        Photo of
+                        <a
+                            href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                            >NASA</a
                         >
-                            <li class="flex items-center space-x-3">
-                                <div
-                                    class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full"
-                                >
-                                    <img src="/icons/medal.png" alt="medal" />
-                                </div>
-                                <span class="text-xl font-medium text-white">
-                                    {{ $t('signUpText1') }}
-                                </span>
-                            </li>
-                            <li class="flex items-center space-x-3">
-                                <div
-                                    class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full"
-                                >
-                                    <img src="/icons/medal.png" alt="medal" />
-                                </div>
-                                <span class="text-xl font-medium text-white">
-                                    {{ $t('signUpText2') }}
-                                </span>
-                            </li>
-                            <li class="flex items-center space-x-3">
-                                <div
-                                    class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full"
-                                >
-                                    <img src="/icons/medal.png" alt="medal" />
-                                </div>
-                                <span class="text-xl font-medium text-white">
-                                    {{ $t('signUpText3') }}
-                                </span>
-                            </li>
-                            <li class="flex items-center space-x-3">
-                                <div
-                                    class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full"
-                                >
-                                    <img src="/icons/medal.png" alt="medal" />
-                                </div>
-                                <span class="text-xl font-medium text-white">
-                                    {{ $t('signUpText4') }}
-                                </span>
-                            </li>
-                        </ul>
+                        on
+                        <a
+                            href="https://unsplash.com/fr/photos/6-jTZysYY_U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                            >Unsplash</a
+                        >
+                    </div>
+                    <div class="absolute inset-0">
+                        <img
+                            class="object-cover w-full h-full"
+                            src="/signup.jpg"
+                            alt="background"
+                        />
+                    </div>
+                    <div
+                        class="absolute inset-0 bg-gradient-to-t from-black to-transparent"
+                    ></div>
+
+                    <div class="relative">
+                        <div
+                            class="w-full max-w-xl xl:w-full xl:mx-auto xl:pr-24 xl:max-w-xl"
+                        >
+                            <h3 class="text-4xl font-bold text-white">
+                                {{ numberOfPeople }} {{ $t('peopleUsing') }}
+                                <br class="hidden xl:block" />
+                                Capitalympics!
+                            </h3>
+                            <ul
+                                class="grid grid-cols-1 mt-10 sm:grid-cols-2 gap-x-8 gap-y-4"
+                            >
+                                <li class="flex items-center space-x-3">
+                                    <div
+                                        class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full"
+                                    >
+                                        <img
+                                            src="/icons/medal.png"
+                                            alt="medal"
+                                        />
+                                    </div>
+                                    <span
+                                        class="text-xl font-medium text-white"
+                                    >
+                                        {{ $t('signUpText1') }}
+                                    </span>
+                                </li>
+                                <li class="flex items-center space-x-3">
+                                    <div
+                                        class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full"
+                                    >
+                                        <img
+                                            src="/icons/medal.png"
+                                            alt="medal"
+                                        />
+                                    </div>
+                                    <span
+                                        class="text-xl font-medium text-white"
+                                    >
+                                        {{ $t('signUpText2') }}
+                                    </span>
+                                </li>
+                                <li class="flex items-center space-x-3">
+                                    <div
+                                        class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full"
+                                    >
+                                        <img
+                                            src="/icons/medal.png"
+                                            alt="medal"
+                                        />
+                                    </div>
+                                    <span
+                                        class="text-xl font-medium text-white"
+                                    >
+                                        {{ $t('signUpText3') }}
+                                    </span>
+                                </li>
+                                <li class="flex items-center space-x-3">
+                                    <div
+                                        class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-full"
+                                    >
+                                        <img
+                                            src="/icons/medal.png"
+                                            alt="medal"
+                                        />
+                                    </div>
+                                    <span
+                                        class="text-xl font-medium text-white"
+                                    >
+                                        {{ $t('signUpText4') }}
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div
-                class="flex items-center justify-center px-4 py-10 sm:px-6 lg:px-8 sm:py-16 lg:py-24 bg-gradient"
-            >
-                <div class="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
-                    <h2
-                        class="text-3xl font-bold leading-tight text-black sm:text-4xl"
-                    >
-                        {{ $t('signUpTo') }} Capitalympics
-                    </h2>
-                    <p class="mt-2 text-base text-black">
-                        {{ $t('alreadyHaveAccount') }}
-                        <RouterLink
-                            to="/login"
-                            class="font-medium text-black italic hover:underline"
+                <div
+                    class="flex items-center justify-center px-4 py-10 sm:px-6 lg:px-8 sm:py-16 lg:py-24 bg-gradient"
+                >
+                    <div class="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
+                        <h2
+                            class="text-3xl font-bold leading-tight text-black sm:text-4xl"
                         >
-                            {{ $t('login') }}
-                        </RouterLink>
-                    </p>
-                    <div class="mt-8 space-y-5">
-                        <TextInput
-                            v-model="name"
-                            :label="$t('name')"
-                            :placeholder="$t('namePlaceholder')"
-                            :prepend-icon="IconUser"
-                            :error="errors.name"
-                        />
-                        <PasswordInput
-                            v-model="password"
-                            :label="$t('password')"
-                            :placeholder="$t('passwordPlaceholder')"
-                            :error="errors.password"
-                            revelable
-                        />
-                        <PasswordInput
-                            v-model="passwordConfirmation"
-                            :label="$t('passwordConfirmation')"
-                            :placeholder="$t('passwordConfirmationPlaceholder')"
-                        />
-                        <Select
-                            v-model="language"
-                            :label="$t('language')"
-                            :options="
-                                languages.map((lang) => ({
-                                    ...lang,
-                                    label: lang.text
-                                }))
-                            "
-                            :prepend-icon="IconLanguage"
-                        />
-                        <div class="pt-2">
-                            <button
-                                type="button"
-                                :disabled="!canTryToSignup || isLoading"
-                                class="w-full flex items-center justify-center px-4 py-4 text-base font-semibold text-black bg-white rounded-md transition-all duration-200 delay-100 hover:scale-105 focus:scale-105 disabled:hover:scale-100 disabled:opacity-60 disabled:cursor-not-allowed outline outline-2 outline-offset-2 outline-transparent focus-visible:outline-blue-600/75"
-                                @click="signup"
+                            {{ $t('signUpTo') }} Capitalympics
+                        </h2>
+                        <p class="mt-2 text-base text-black">
+                            {{ $t('alreadyHaveAccount') }}
+                            <RouterLink
+                                to="/login"
+                                class="font-medium text-black italic hover:underline"
                             >
-                                <span v-if="!isLoading">{{
-                                    $t('signup')
-                                }}</span>
-                                <div
-                                    v-else
-                                    class="h-5 w-5 border-4 rounded-full border-blue-600/75 border-b-blue-600/25 animate-spin"
-                                ></div>
-                            </button>
+                                {{ $t('login') }}
+                            </RouterLink>
+                        </p>
+                        <div class="mt-8 space-y-5">
+                            <TextInput
+                                v-model="name"
+                                :label="$t('name')"
+                                :placeholder="$t('namePlaceholder')"
+                                :prepend-icon="IconUser"
+                                :error="errors.name"
+                            />
+                            <PasswordInput
+                                v-model="password"
+                                :label="$t('password')"
+                                :placeholder="$t('passwordPlaceholder')"
+                                :error="errors.password"
+                                revelable
+                            />
+                            <PasswordInput
+                                v-model="passwordConfirmation"
+                                :label="$t('passwordConfirmation')"
+                                :placeholder="
+                                    $t('passwordConfirmationPlaceholder')
+                                "
+                            />
+                            <Select
+                                v-model="language"
+                                :label="$t('language')"
+                                :options="
+                                    languages.map((lang) => ({
+                                        ...lang,
+                                        label: lang.text
+                                    }))
+                                "
+                                :prepend-icon="IconLanguage"
+                            />
+                            <div class="pt-2">
+                                <button
+                                    type="button"
+                                    :disabled="!canTryToSignup || isLoading"
+                                    class="w-full flex items-center justify-center px-4 py-4 text-base font-semibold text-black bg-white rounded-md transition-all duration-200 delay-100 hover:scale-105 focus:scale-105 disabled:hover:scale-100 disabled:opacity-60 disabled:cursor-not-allowed outline outline-2 outline-offset-2 outline-transparent focus-visible:outline-blue-600/75"
+                                    @click="signup"
+                                >
+                                    <span v-if="!isLoading">{{
+                                        $t('signup')
+                                    }}</span>
+                                    <div
+                                        v-else
+                                        class="h-5 w-5 border-4 rounded-full border-blue-600/75 border-b-blue-600/25 animate-spin"
+                                    ></div>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
